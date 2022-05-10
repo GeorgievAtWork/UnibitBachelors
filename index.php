650f13badc07be9804980ca6c3a75f28e56a5ee4
GeorgievAtWork <georgigatwork@gmail.com>
2022-05-10 22:58:21 +0300

    Landing page WIP

---
+++ index.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="Description" content="Enter your description here" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <title>Expansio</title>
+</head>
+
+<body>
+    <section id="home" class="container-fluid background">
+        <nav class="navbar navbar-expand-md navbar-dark ">
+            <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Категории</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Учебни пътеки</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="mx-auto order-0">
+                <a class="navbar-brand mx-auto" href="#"><img src='assets/images/logo.png' width="150"
+                        height="150" /></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+            <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">За нас</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Контакти</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container pushdown left-text-cont">
+                <h1 class="display-4">Добре дошли в Expansio</h1>
+                <p class="lead">Какво искате да научите днес?</p>
+            </div>
+        </div>
+        <div class="row ">
+            <div class="col-sm-12 see-more">
+                <a href="#about">Вижте повече <i class="fas fa-angle-double-down"></i> </a>
+            </div>
+        </div>
+    </section>
+
+    <section id="about" class="container-fluid about-container">
+        <div class="row title-holder">
+            <div class="col-md-12">
+                <h2 class="title" data-aos="fade-down">Защо Expansio?</h2>
+            </div>
+        </div>
+        <div class="row ">
+            <div class="col-md-12 ">
+                <p class="about-text" data-aos="fade-up">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore soluta accusamus temporibus
+                    assumenda tempore a deleniti suscipit, eum repellat sequi error explicabo hic ducimus officiis
+                    iusto? Nam, dignissimos voluptate! Aperiam.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore soluta accusamus temporibus
+                    assumenda tempore a deleniti suscipit, eum repellat sequi error explicabo hic ducimus officiis
+                    iusto? Nam, dignissimos voluptate! Aperiam.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore soluta accusamus temporibus
+                    assumenda tempore a deleniti suscipit, eum repellat sequi error explicabo hic ducimus officiis
+                    iusto? Nam, dignissimos voluptate! Aperiam.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore soluta accusamus temporibus
+                    assumenda tempore a deleniti suscipit, eum repellat sequi error explicabo hic ducimus officiis
+                    iusto? Nam, dignissimos voluptate! Aperiam.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore soluta accusamus temporibus
+                    assumenda tempore a deleniti suscipit, eum repellat sequi error explicabo hic ducimus officiis
+                    iusto? Nam, dignissimos voluptate! Aperiam.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore soluta accusamus temporibus
+                    assumenda tempore a deleniti suscipit, eum repellat sequi error explicabo hic ducimus officiis
+                    iusto? Nam, dignissimos voluptate! Aperiam.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore soluta accusamus temporibus
+                    assumenda tempore a deleniti suscipit, eum repellat sequi error explicabo hic ducimus officiis
+                    iusto? Nam, dignissimos voluptate! Aperiam.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore soluta accusamus temporibus
+                    assumenda tempore a deleniti suscipit, eum repellat sequi error explicabo hic ducimus officiis
+                    iusto? Nam, dignissimos voluptate! Aperiam.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore soluta accusamus temporibus
+                    assumenda tempore a deleniti suscipit, eum repellat sequi error explicabo hic ducimus officiis
+                    iusto? Nam, dignissimos voluptate! Aperiam.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore soluta accusamus temporibus
+                    assumenda tempore a deleniti suscipit, eum repellat sequi error explicabo hic ducimus officiis
+                    iusto? Nam, dignissimos voluptate! Aperiam.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore soluta accusamus temporibus
+                    assumenda tempore a deleniti suscipit, eum repellat sequi error explicabo hic ducimus officiis
+                    iusto? Nam, dignissimos voluptate! Aperiam.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore soluta accusamus temporibus
+                    assumenda tempore a deleniti suscipit, eum repellat sequi error explicabo hic ducimus officiis
+                    iusto? Nam, dignissimos voluptate! Aperiam.
+                </p>
+            </div>
+        </div>
+
+        <div class="row img-container">
+            <div class="col-md-4">
+                <img src="assets/images/about-1.png" alt="image1" class="oval-img right" data-aos="fade-right"
+                    data-aos-duration="1000" />
+            </div>
+            <div class="col-md-4">
+                <img src="assets/images/about-2.png" alt="image2" data-aos="fade-up" data-aos-duration="750"
+                    data-aos-delay="150" width="300" height="300" />
+            </div>
+            <div class="col-md-4">
+                <img src="assets/images/about-3.png" alt="image3" class="oval-img left wider" data-aos="fade-left"
+                    data-aos-duration="1000" data-aos-delay="300" />
+            </div>
+        </div>
+    </section>
+
+    <section id="categories" class="container-fluid categories-container">
+        <div class="row title-holder">
+            <div class="col-md-12">
+                <h2 class="title white" data-aos="fade-down">Категории</h2>
+            </div>
+            </row>
+            <div class="container">
+                <div class="row categories-holder">
+                    <div class="col-sm-4 category-box" data-aos="fade-right">
+                        <a href="#">
+                            <img src="assets/images/courses-it.png" class="img-holder" />
+                            Информационни технологии</a>
+                    </div>
+                    <div class="col-sm-4 category-box" data-aos="fade-down">
+                        <a href="#">
+                            <img src="assets/images/courses-lifestyle.png" class="img-holder" />
+                            Лайфстайл</a>
+                    </div>
+                    <div class="col-sm-4 category-box" data-aos="fade-left">
+                        <a href="#">
+                            <img src="assets/images/courses-finances.png" class="img-holder" />
+                            Финанси</a>
+                    </div>
+                    <div class="w-100"></div>
+                    <div class="col-sm-4 category-box" data-aos="fade-right">
+                        <a href="#">
+                            <img src="assets/images/courses-sciences.png" class="img-holder" />
+                            Наука</a>
+                    </div>
+                    <div class="col-sm-4 category-box" data-aos="fade-up">
+                        <a href="#">
+                            <img src="assets/images/courses-photography.png" class="img-holder" />
+                            Фотография</a>
+                    </div>
+                    <div class="col-sm-4 category-box" data-aos="fade-left">
+                        <a href="#">
+                            <img src="assets/images/courses-fitness.png" class="img-holder" />
+                            Фитнес и тренировки</a>
+                    </div>
+                </div>
+            </div>
+
+    </section>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="assets/js/script.js"></script>
+
+</body>
+
+</html>
