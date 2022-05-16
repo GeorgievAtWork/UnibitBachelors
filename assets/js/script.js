479dd14c0c85aa28b0d08cdaa6464dc995f11f3a
GeorgievAtWork <georgigatwork@gmail.com>
2022-05-16 18:15:14 +0300

    Finished main page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
 AOS.init();
+
+// Scroll to top button
+
+const btnScrollToTop = document.querySelector(".scrollToTopBtn");
+
+window.addEventListener("scroll", () => { 
+  if (window.pageYOffset > 100) { 
+    btnScrollToTop.classList.add("active"); 
+  } else { 
+    btnScrollToTop.classList.remove("active"); 
+  } 
+});
+
--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 </head>
 
 <body>
-    <a href="#home" class="to-top"><i class="fa fa-long-arrow-up"></i></a>
+    <a href="#home" class="scrollToTopBtn"><i class="fa fa-long-arrow-up "></i></a>
     <section id="home" class="container-fluid background">
         <nav class="navbar navbar-expand-md navbar-dark ">
             <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
@@ -40,7 +40,7 @@
                         <a class="nav-link" href="#about">За нас</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Контакти</a>
+                        <a class="nav-link" href="#contacts">Контакти</a>
                     </li>
                 </ul>
             </div>
@@ -66,6 +66,7 @@
                 <h2 class="title" data-aos="fade-down" data-aos-duration="750" data-aos-delay="150">Защо Expansio?</h2>
             </div>
         </div>
+        <div class="container">
         <div class="row ">
             <div class="col-md-12 ">
                 <p class="about-text" data-aos="fade-up" data-aos-duration="750" data-aos-delay="150">
@@ -85,43 +86,25 @@
                     assumenda tempore a deleniti suscipit, eum repellat sequi error explicabo hic ducimus officiis
                     iusto? Nam, dignissimos voluptate! Aperiam.
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore soluta accusamus temporibus
-                    assumenda tempore a deleniti suscipit, eum repellat sequi error explicabo hic ducimus officiis
-                    iusto? Nam, dignissimos voluptate! Aperiam.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore soluta accusamus temporibus
-                    assumenda tempore a deleniti suscipit, eum repellat sequi error explicabo hic ducimus officiis
-                    iusto? Nam, dignissimos voluptate! Aperiam.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore soluta accusamus temporibus
-                    assumenda tempore a deleniti suscipit, eum repellat sequi error explicabo hic ducimus officiis
-                    iusto? Nam, dignissimos voluptate! Aperiam.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore soluta accusamus temporibus
-                    assumenda tempore a deleniti suscipit, eum repellat sequi error explicabo hic ducimus officiis
-                    iusto? Nam, dignissimos voluptate! Aperiam.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore soluta accusamus temporibus
-                    assumenda tempore a deleniti suscipit, eum repellat sequi error explicabo hic ducimus officiis
-                    iusto? Nam, dignissimos voluptate! Aperiam.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore soluta accusamus temporibus
-                    assumenda tempore a deleniti suscipit, eum repellat sequi error explicabo hic ducimus officiis
-                    iusto? Nam, dignissimos voluptate! Aperiam.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore soluta accusamus temporibus
-                    assumenda tempore a deleniti suscipit, eum repellat sequi error explicabo hic ducimus officiis
-                    iusto? Nam, dignissimos voluptate! Aperiam.
+                    a
                 </p>
             </div>
         </div>
-
+        
         <div class="row img-container">
             <div class="col-md-4">
                 <img src="assets/images/about-1.png" alt="image1" class="oval-img right" data-aos="fade-right" data-aos-duration="1000" />
-                <span class="hidden">Искаш да научиш нещо ново?</span>
+                <span class="caption" data-aos="zoom-in" data-aos-delay="800" data-aos-anchor="#about">Искаш да научиш нещо ново?</span>
             </div>
             <div class="col-md-4">
                 <img src="assets/images/about-2.png" class="oval-img" alt="image2" data-aos="fade-up" data-aos-duration="750" data-aos-delay="150" width="300" height="300" />
-                <span class="hidden">Избери от категориите в сайта и започни да учиш с видеоуроци</span>
+                <span class="caption" data-aos="zoom-in" data-aos-delay="800" data-aos-anchor="#about">Избери от категориите в сайта и започни да учиш с видеоуроци</span>
             </div>
             <div class="col-md-4">
                 <img src="assets/images/about-3.png" alt="image3" class="oval-img left wider" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="300" />
-                <span class="hidden">Придобий нови знания и се сертифицирай</span>
+                <span class="caption" data-aos="zoom-in" data-aos-delay="800" data-aos-anchor="#about">Придобий нови знания и се сертифицирай</span>
             </div>
+        </div>
         </div>
     </section>
 
@@ -132,7 +115,7 @@
             </div>
             </row>
             <div class="container">
-                <div class="row content-holder">
+                <div class="row categories-holder">
                     <div class="col-sm-4 category-box" data-aos="fade-right">
                         <a href="#">
                             <img src="assets/images/courses-it.png" class="img-holder" />
@@ -175,31 +158,62 @@
             </div>
             </row>
             <div class="container">
-                <div class="row content-holder tutors-holder">
+                <div class="row tutors-holder">
                     <div class="col-xl-4 tutor-box" data-aos="fade-right">
                         <a href="#">
                             <img src="assets/images/tutor-1.png" class="img-holder" />
                             <span class="name-holder">John Doe</span>
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, iusto ad rerum vero tempora distinctio a saepe sint veniam sequi expedita odio, explicabo libero autem alias dolor. Consectetur, recusandae error.</p>
-                            </a>
-                        </div>
+                        </a>
+                    </div>
                     <div class="col-xl-4 tutor-box " data-aos="fade-up">
                         <a href="#">
                             <img src="assets/images/tutor-2.png" class="img-holder" />
                             <span class="name-holder">John Doe</span>
                             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit quaerat cumque temporibus officia molestiae voluptatum deleniti earum accusamus enim quod nisi deserunt reprehenderit sint veritatis modi, corrupti voluptatem fuga repellendus.</p>
-                            </a>
-                        </div>
+                        </a>
+                    </div>
                     <div class="col-xl-4 tutor-box" data-aos="fade-left">
                         <a href="#">
                             <img src="assets/images/tutor-3.png" class="img-holder" />
                             <span class="name-holder">John Doe</span>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga non deserunt provident voluptatibus enim culpa, blanditiis architecto autem sed adipisci esse maxime velit omnis optio cum atque praesentium voluptate ab!</p>
-                            </a>
-                        </div>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
+    </section>
+
+    <section id="contacts" class="container-fluid section-container contacts-container">
+        <div class="row title-holder">
+            <div class="col-md-12">
+                <h2 class="title" data-aos="fade-down" data-aos-delay="100">Контакти</h2>
+            </div>
+
+
+            <div class="container content-holder">
+                <div class="row contacts-holder">
+                    <div class="col-xl-3 location-data" data-aos="fade-right" data-aos-delay="200">
+                        <h4>Адрес</h4>
+                        <p>България, София, ж.к. Lorem Ipsum, улица Ipsum Lorem №: 2</p>
+
+                        <h4>Телефон</h4>
+                        <p>0888888888</p>
+
+                        <h4> Email </h4>
+                        <p>lorem.ipsum@lipsum.com</p>
+                    </div>
+                    <div class="col-xl-1 offset-xl-1"></div>
+                    <div class="col-xl-7 location-data" data-aos="fade-left" data-aos-delay="300">
+                        <iframe id="gmap_canvas" src="https://maps.google.com/maps?q=2880%20Broadway,%20New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <p class="trademark">Създадено от Георги Георгиев - 46156р, като част от практическата разработка за дипломна работа<br/> Ниво - Бакалавър</p>
+        
     </section>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
