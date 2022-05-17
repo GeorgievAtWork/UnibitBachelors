356f4bd44d330164ff6ca46da7b0ee26bb14bc14
GeorgievAtWork <georgigatwork@gmail.com>
2022-05-17 05:13:59 +0300

    Finished all internal dynamic pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,21 @@ AOS.init();
 
 const btnScrollToTop = document.querySelector(".scrollToTopBtn");
 
-window.addEventListener("scroll", () => { 
-  if (window.pageYOffset > 100) { 
-    btnScrollToTop.classList.add("active"); 
-  } else { 
-    btnScrollToTop.classList.remove("active"); 
-  } 
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+        btnScrollToTop.classList.add("active");
+    } else {
+        btnScrollToTop.classList.remove("active");
+    }
 });
 
+
+function redirectCategory(num) {
+    $('#category-form').append('<input type="hidden" name="category_id" value="' + num + '" />');
+    $('#category-form').submit();
+}
+
+function redirectTutor(num) {
+    $('#tutor-form').append('<input type="hidden" name="tutor_id" value="' + num + '" />');
+    $('#tutor-form').submit();
+}
