b75373d21d3083958c220df445ad92c8ebe3635f
GeorgievAtWork <georgigatwork@gmail.com>
2022-05-17 05:20:06 +0300

    Clearing whitespaces

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 AOS.init();
 
+
 // Scroll to top button
 
 const btnScrollToTop = document.querySelector(".scrollToTopBtn");
@@ -13,10 +14,15 @@ window.addEventListener("scroll", () => {
 });
 
 
+// Redirect to Category page function
+
 function redirectCategory(num) {
     $('#category-form').append('<input type="hidden" name="category_id" value="' + num + '" />');
     $('#category-form').submit();
 }
+
+
+//Redirect to Tutor page function
 
 function redirectTutor(num) {
     $('#tutor-form').append('<input type="hidden" name="tutor_id" value="' + num + '" />');
