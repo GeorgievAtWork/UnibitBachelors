2771689d16b8678204f23d9388e6b4137bef8556
GeorgievAtWork <georgigatwork@gmail.com>
2022-05-23 09:35:36 +0300

    Changes in SQL init

--- src/assets/sql/init.sql
+++ src/assets/sql/init.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 23, 2022 at 08:24 AM
+-- Generation Time: May 23, 2022 at 08:35 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.0
 
